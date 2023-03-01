272565891ac5f5c9ee7fc0283760ad54b999be03
nanyamax <ifunanyasunday7@gmail.com>
2023-03-01 12:41:25 -0800

    binarytree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,27 +1,24 @@
-#include <binary_trees.h>
+#include "binary_trees.h"
 
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
+ * binary_tree_node - Create a binary node
+ * @parent: node parent
+ * @value: Value of the node
+ * Return: the new node
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t))
-	if(value == NULL)
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
 	{
-		return(NULL)
+		return (NULL);
 	}
-	new_node -> n = value;
-	new_node -> parent = parent;
-	new_node -> left = NULL;
-	new_node -> right = NULL;
-	return new_node;
-
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
 }
